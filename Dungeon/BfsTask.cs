c276f71d91635824ef087b58156b6f79353ca100
К Б <yabkn@outlook.com>
2023-04-24 20:25:02 +0700

    DungeonTask have done

--- Dungeon/BfsTask.cs
+++ Dungeon/BfsTask.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel;
 using System.Drawing;
+using System.Linq;
 
 
 namespace Dungeon
@@ -8,64 +10,114 @@ namespace Dungeon
     {
         public static IEnumerable<SinglyLinkedList<Point>> FindPaths(Map map, Point start, Point[] chests)
         {
-            //var list = new List<SinglyLinkedList<Point>>();
+            Dictionary<Point, SinglyLinkedList<Point>> path = new Dictionary<Point, SinglyLinkedList<Point>>();
+            SinglyLinkedList<Point> pathCurrentChest=null;
+            for (int i = 0; i < chests.Length; i++) //пробегаемся по сундукам
+            {
+                if (path.ContainsKey(chests[i])) //проверяем не попадался ли нам ранее такой сундук
+                {
+                    yield return path[chests[i]];
+                    continue;
+                }
+                else
+                {
+                    pathCurrentChest = FindPath(map, start, chests[i]);
+                    path.Add(chests[i], pathCurrentChest);
+                }
+                if (pathCurrentChest!=null) yield return pathCurrentChest;
+            }
+            yield break;
+        }
+
+
+        public static SinglyLinkedList<Point> FindPath(Map map, Point start, Point chests)
+        {
             var direction = new int[,] { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
             SinglyLinkedList<Point> current = null;
-            var queue = new Queue<SinglyLinkedList<Point>>();//помещаем в очередь не только точку но и путь по которому сюда пришли
-            var visited = new HashSet<Point>();//помечаем точки в которых уже искали возможные пути
-            var queueVisited = new HashSet<Point>();//отбрасываем лишние пути в эту точку если эту точку уже помещали в стэк
+            var queue =
+                new Queue<SinglyLinkedList<Point>>(); //помещаем в очередь не только точку но и путь по которому сюда пришли
+            var queueVisited =
+                new HashSet<Point>(); //отбрасываем лишние пути в эту точку если эту точку уже помещали в стэк
             queueVisited.Add(start);
             //var sourcedPath = new HashSet<Point>();
             queue.Enqueue(new SinglyLinkedList<Point>(start));
-            while (queue.Count != 0)//в очередь постепенно кладутся точки которые мы будем использовать на следующем шаге
+
+            while
+                (queue.Count != 0) //в очередь постепенно кладутся точки которые мы будем использовать на следующем шаге
             {
                 var point = queue.Dequeue();
                 if (map.Dungeon[point.Value.X, point.Value.Y] == MapCell.Wall) continue;
-                for (int i = 0; i < chests.Length; i++)
-                { 
-                    //if (point.Value.Equals(chests[i]) && !sourcedPath.Contains(chests[i]))
-                    if (point.Value.Equals(chests[i]))
-                    {
-                        //sourcedPath.Add(point.Value);
-                        //list.Add(point);
-                        yield return point;
-                    }
+                if (point.Value.Equals(chests))
+                {
+                    return point;
                 }
+
                 current = new SinglyLinkedList<Point>(point.Value, current);
-                visited.Add(current.Value);
                 //for (var dy = -1; dy <= 1; dy++)
                 //    for (var dx = -1; dx <= 1; dx++)
                 //        if (dx != 0 && dy != 0) continue;
                 //        else
                 for (int j = 0; j <= 3; j++)
                 {
-                    var newPoint = new Point { X = point.Value.X + direction[j,0], Y = point.Value.Y + +direction[j, 1] };
-                            if (map.InBounds(newPoint) && !queueVisited.Contains(newPoint) && !visited.Contains(newPoint))
-                            {
-                                queueVisited.Add(newPoint);
-                                queue.Enqueue(new SinglyLinkedList<Point>(newPoint, point));
-                            }
-                 }
+                    var newPoint = new Point
+                    { X = point.Value.X + direction[j, 0], Y = point.Value.Y + +direction[j, 1] };
+                    if (map.InBounds(newPoint) && !queueVisited.Contains(newPoint))
+                    {
+                        queueVisited.Add(newPoint);
+                        queue.Enqueue(new SinglyLinkedList<Point>(newPoint, point));
+                    }
+                }
             }
-            //if (list.Count > 0)
-            //{
-            //    for (int i = 0; i < chests.Length; i++) //выбираем самый короткий путь до сундука
-            //    {
-            //        var bestPice = double.PositiveInfinity;
-            //        SinglyLinkedList<Point> best = null;
-            //        foreach (var item in list)
-            //        {
-            //            if (item.Value.Equals(chests[i]))
-            //                if (item.Length < bestPice)
-            //                    best = item;
-            //        }
-            //        yield return best;
-            //    }
-            //}
-            yield break;
+            return null;
         }
+
+
+        //if (list.Count > 0)
+        //{
+        //    for (int i = 0; i < chests.Length; i++) //выбираем самый короткий путь до сундука
+        //    {
+        //        var bestPice = double.PositiveInfinity;
+        //        SinglyLinkedList<Point> best = null;
+        //        foreach (var item in list)
+        //        {
+        //            if (item.Value.Equals(chests[i]))
+        //                if (item.Length < bestPice)
+        //                    best = item;
+        //        }
+        //        yield return best;
+        //    }
+        //}
+
+        //public class BfsTask
+        //{
+        //    public static IEnumerable<SinglyLinkedList<Point>> FindPaths(Map map, Point start, Point[] chests)
+        //    {
+        //        //public static IEnumerable<Node> BreadthSearch(this Node startNode)//сделали расширением у класса Node
+        //        {
+        //            // Внимание! Перед использованием этого кода, прочитайте следующий слайд «Использование памяти». Это обход в ширину
+        //            var visited = new HashSet<Point>();//посещенные вершины
+        //            //var queue = new Queue<Point>();
+        //            var queue = new Queue<SinglyLinkedList<Point>>();
+        //            //queue.Enqueue(start);
+        //            queue.Enqueue(new SinglyLinkedList<Point>(start));
+        //            while (queue.Count != 0)
+        //            {
+        //                var point = queue.Dequeue();
+        //                if (map.Dungeon[point.Value.X, point.Value.Y] == MapCell.Wall) continue;
+        //                if (visited.Contains(node)) continue; //если мы уже были в этой вершине, то повторно ее не рассматриваем
+        //                visited.Add(node);//добавляем в список просмотренных
+        //                yield return node;
+        //                foreach (var incidentNode in node.IncidentNodes)
+        //                    queue.Enqueue(incidentNode);
+        //            }
+        //        }
+
+        //    }
+        //}
     }
 }
+
+
 
 //using System;
 //using System.Collections.Generic;
@@ -76,44 +128,45 @@ namespace Dungeon
 
 //namespace Dungeon
 //{
-//    public class BfsTask
-//    {
-//        public static IEnumerable<SinglyLinkedList<Point>> FindPaths(Map map, Point start, Point[] chests)
-//        {
-//            var track = new Dictionary<Point, SinglyLinkedList<Point>>();
-//            track[start] = new SinglyLinkedList<Point>(start);
-//            var queue = new Queue<SinglyLinkedList<Point>>();
-//            queue.Enqueue(track[start]);
-//            foreach (var chest in chests)
-//            {
-//                if (track.ContainsKey(chest))
-//                {
-//                    yield return track[chest];
-//                    continue;
-//                }
-//                var path = FindPath(track, queue, map, start, chest);
-//                if (path != null)
-//                    yield return path;
-//            }
-//        }
-//        static SinglyLinkedList<Point> FindPath(Dictionary<Point, SinglyLinkedList<Point>> track, Queue<SinglyLinkedList<Point>> queue, Map map, Point start, Point end)
-//        {
-//            while (queue.Count != 0)
-//            {
-//                var node = queue.Dequeue();
-//                var incidentNodes = Walker.PossibleDirections
-//                    .Select(direction => node.Value + direction)
-//                    .Where(point => map.InBounds(point) && map.Dungeon[point.X, point.Y] != MapCell.Wall);
-//                foreach (var nextNode in incidentNodes)
-//                {
-//                    if (track.ContainsKey(nextNode)) continue;
-//                    track[nextNode] = new SinglyLinkedList<Point>(nextNode, node);
-//                    queue.Enqueue(track[nextNode]);
-//                }
-//                if (track.ContainsKey(end)) return track[end];
-//            }
-//            if (!track.ContainsKey(end)) return null;
-//            throw new Exception("There should never be this exception");
-//        }
-//    }
+//	public class BfsTask
+//	{
+//		public static IEnumerable<SinglyLinkedList<Point>> FindPaths(Map map, Point start, Point[] chests)
+//		{
+//			var track = new Dictionary<Point, SinglyLinkedList<Point>>();
+//			track[start] = new SinglyLinkedList<Point>(start);
+//			var queue = new Queue<SinglyLinkedList<Point>>();
+//			queue.Enqueue(track[start]);
+//			foreach (var chest in chests)
+//			{
+//				if (track.ContainsKey(chest))
+//				{
+//					yield return track[chest];
+//					continue;
+//				}
+//				var path = FindPath(track, queue, map, start, chest);
+//				if (path != null)
+//					yield return path;
+//			}
+//		}
+//		static SinglyLinkedList<Point> FindPath(Dictionary<Point, SinglyLinkedList<Point>> track, Queue<SinglyLinkedList<Point>> queue, Map map, Point start, Point end)
+//		{
+//			while (queue.Count != 0)
+//			{
+//				var node = queue.Dequeue();
+//				var incidentNodes = Walker.PossibleDirections
+//					.Select(direction => node.Value + direction)
+//					.Where(point => map.InBounds(point) && map.Dungeon[point.X, point.Y] != MapCell.Wall);
+//				foreach (var nextNode in incidentNodes)
+//				{
+//					if (track.ContainsKey(nextNode)) continue;
+//					track[nextNode] = new SinglyLinkedList<Point>(nextNode, node);
+//					queue.Enqueue(track[nextNode]);
+//				}
+//				if (track.ContainsKey(end)) return track[end];
+//			}
+//			if (!track.ContainsKey(end)) return null;
+//			throw new Exception("There should never be this exception");
+//		}
+//	}
 //}
+
--- Dungeon/DungeonTask.cs
+++ Dungeon/DungeonTask.cs
@@ -10,9 +10,95 @@ namespace Dungeon
 {
 	public class DungeonTask
 	{
-		public static MoveDirection[] FindShortestPath(Map map)
+        public static MoveDirection[] FindShortestPath(Map map)
 		{
-			return new MoveDirection[0];
-		}
+            
+
+            var pathsToChests = BfsTask.FindPaths(map, map.InitialPosition, map.Chests)
+                .Select(x => x.ToList())
+                .ToArray();
+            var pathsFromExitToChests = BfsTask.FindPaths(map, map.Exit, map.Chests)
+                .Select(x => x.ToList())
+                .ToArray();
+            //if (pathsToChests.Length == 0|| pathsFromExitToChests.Length == 0) 
+            //    return new MoveDirection[0];
+            if (pathsToChests.Length == 0)
+            {
+                
+                var pathsToExit = BfsTask.FindPaths(map, map.InitialPosition,
+                        new Point[] { map.Exit })
+                    .Select(x => x.ToList())
+                    .ToArray();
+                if (pathsToExit.Length == 0)
+                return new MoveDirection[0];
+                    int count = pathsToExit[0].Count();
+                MoveDirection[] direction2 = new MoveDirection[count-1];
+                for (int i = pathsToExit[0].Count - 1; i > 0; i--)
+                {
+                    int coordinateX = pathsToExit[0][i - 1].X - pathsToExit[0][i].X;
+                    int coordinateY = pathsToExit[0][i - 1].Y - pathsToExit[0][i].Y;
+                    direction2[pathsToExit[0].Count - 1 - i] = Walker.ConvertOffsetToDirection(new Size(coordinateX, coordinateY));
+                    //dir[i, 0] = coordinateX;
+                    //dir[i, 1] = coordinateY;
+                }
+                return direction2;
+            }
+            int min = 0;
+            int minItem1 = 0;
+            int minItem2 = 0;
+            for (int i = 0; i < pathsToChests.Length; i++)
+            {
+                for (int j = 0; j < pathsFromExitToChests.Length; j++)
+                {
+                    if (pathsToChests[i][0] == pathsFromExitToChests[j][0])
+                    {
+                        if (min == 0)
+                        {
+                            min = pathsToChests[i].Count + pathsFromExitToChests[j].Count;
+                            minItem1 = i;
+                            minItem2 = j;
+                        }
+                        int current = pathsToChests[i].Count + pathsFromExitToChests[j].Count;
+                        if (current < min)
+                        {
+                            min = current;
+                            minItem1 = i;
+                            minItem2 = j;
+                        }
+                    }
+                }
+            }
+            if (min==0) return new MoveDirection[0];
+            MoveDirection[] direction=new MoveDirection[min-2];
+            for (int i = pathsToChests[minItem1].Count-1; i > 0; i--)
+            {
+                int coordinateX = pathsToChests[minItem1][i-1].X - pathsToChests[minItem1][i].X;
+                int coordinateY = pathsToChests[minItem1][i-1].Y - pathsToChests[minItem1][i].Y;
+                direction[pathsToChests[minItem1].Count-1-i] = Walker.ConvertOffsetToDirection(new Size(coordinateX, coordinateY));
+            }
+            direction[pathsToChests[minItem1].Count - 1]= Walker.ConvertOffsetToDirection(new Size(
+                pathsFromExitToChests[minItem2][1].X- pathsToChests[minItem1][0].X,
+                pathsFromExitToChests[minItem2][1].Y - pathsToChests[minItem1][0].Y));
+            for (int i = 0; i < pathsFromExitToChests[minItem2].Count - 1; i++)
+            {
+                int coordinateX = pathsFromExitToChests[minItem2][i+1].X - pathsFromExitToChests[minItem2][i].X;
+                int coordinateY = pathsFromExitToChests[minItem2][i+1].Y - pathsFromExitToChests[minItem2][i].Y;
+                direction[i+ pathsToChests[minItem2].Count-1] = Walker.ConvertOffsetToDirection(new Size(coordinateX, coordinateY));
+            }
+            return direction;
+        }
+
+
 	}
 }
+
+//direction[0] = MoveDirection.Down;
+//direction[1] = MoveDirection.Down;
+//direction[2] = MoveDirection.Right;
+//direction[3] = MoveDirection.Right;
+//direction[4] = MoveDirection.Right;
+//direction[5] = MoveDirection.Right;
+//direction[6] = MoveDirection.Down;
+//direction[7] = MoveDirection.Down;
+//direction[8] = MoveDirection.Left;
+//direction[9] = MoveDirection.Left;
